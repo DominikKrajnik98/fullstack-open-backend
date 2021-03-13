f10aa8779c30eef5e2f41a57de9ff6f25448a97e
Dominik Krajnik <dominik@bmk-it.com>
2021-03-13 22:09:26 +0100

    uibuild

--- src/models/person.js
+++ src/models/person.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
+const url = 'mongodb+srv://fullstack:QyCtCDBYFOYqM3bQ@cluster0.zba7l.mongodb.net/note-app?retryWrites=true'
 
 console.log('connecting to', url)
 
